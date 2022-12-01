7d9c4aa161928228314eb171a730083bc936ecd5
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-11-30 21:34:25 -0300

    Add files via upload

--- oficina/pecas.php
+++ oficina/pecas.php
@@ -4,47 +4,64 @@ require_once 'menu.php';
 ?>
 
 <div class="container-fluid">
-    <div class="row">
+    <div class="row pecas">
         <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+            <div class="card">
+                <img class="card-img-top" src="imagens/vela.jpg" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Vela NGK</h5>
+                    <p class="card-text">A Ngk é referência no mercado de produtos automotivos. Vela de auto desempenho para veículos flex.</p>
+                    <p>R$ 60,00</p>
+                    <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vela">
+  Comprar
+</button>
+                    
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+            <div class="card">
+                <img class="card-img-top" src="imagens/bobina.jpg" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Bobinas Magnet Marelli</h5>
+                    <p class="card-text">Alto desempenho e energia para seu carro.</p>
+                    <p>R$ 250,00</p>
+                    <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#bobina">
+  Comprar
+</button>
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+            <div class="card">
+                <img class="card-img-top" src="imagens/bieleta.jpg" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Bieleta Nakata</h5>
+                    <p class="card-text">Bieleta Nakata - Ande sempre com segurança e estabilidade.</p>
+                    <p>R$ 70,00</p>
+                   <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#bieleta">
+  Comprar
+</button>
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+            <div class="card" >
+                <img class="card-img-top" src="imagens/amortecedor.jpg" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Amortecedores Cofap</h5>
+                    <p class="card-text">A cofap é referência no ramos de peças de suspensão. Deixe seu carro estável e seguro.</p>
+                    </p>R$ 560,00</p>
+                    <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#amortecedor">
+  Comprar
+</button>
                 </div>
             </div>
         </div>
@@ -56,6 +73,95 @@ require_once 'menu.php';
 
 
 
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="vela" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Vela NGK</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="imagens/vela.jpg">
+        <h4>Velas NGK</h4>
+        <p>Soluções inteligentes para seu carro</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Adicionar ao Carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="bobina" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Adicionar ao Carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="amortecedor" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Adicionar ao Carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="bieleta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Adicionar ao Carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <?php
