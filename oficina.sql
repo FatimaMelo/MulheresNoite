bff394782d03de9519ab4026bc5655e296d29072
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-12-08 21:27:38 -0300

    Add files via upload

--- oficina.sql
+++ oficina.sql
@@ -1,4 +1,6 @@
-create database oficina;
+drop database if exists oficina;
+
+create database oficina character set utf8 collate utf8_general_ci;
 
 use oficina;
 
@@ -19,7 +21,7 @@ create table cliente(
 	foreign key(cep)references endereco(cep));
 	
 create table funcionario(
-	matricula integer primary key,
+	matricula integer primary key auto_increment,
 	nome varchar(60)not null,
 	telefone varchar(15)not null,
 	cpf char(12)not null,
@@ -30,6 +32,7 @@ create table funcionario(
 	complemento varchar(30),
 	foreign key(cep)references endereco(cep));
 	
+	
 	create table veiculo(
 		placa char(7)primary key,
 		marca varchar(20)not null,
@@ -38,6 +41,58 @@ create table funcionario(
 		ano char(4)not null,
 		cpf char(12)not null,
 		foreign key(cpf)references cliente(cpf));
+		
+	create table servico(
+		ordemservico integer primary key auto_increment,
+		defeito varchar(100)not null,
+		dataentrada date not null,
+		datasaida date not null,
+		preco double not null,
+		garantia varchar(50)not null,
+		formapagamento varchar(20)not null,
+		placa char(7)not null,
+		foreign key(placa)references veiculo(placa));
+		
+	create table peca(
+		codigopeca integer primary key auto_increment,
+		nome varchar(60)not null,
+		marca varchar(40) not null,
+		modeloano varchar(250)not null,
+		quantidade integer not null,
+		preco double not null);
+		
+	create table servicopeca(
+		idservicopeca integer primary key auto_increment,
+		quantidade integer not null,
+		codigopeca integer not null,
+		ordemservico integer not null,
+		foreign key(codigopeca)references peca(codigopeca),
+		foreign key(ordemservico)references servico(ordemservico));
+		
+	create table servicofunc(
+		idservicofunc integer primary key auto_increment,
+		descricao varchar(100)not null,
+		ordemservico integer not null,
+		matricula integer not null,
+		foreign key(ordemservico)references servico(ordemservico),
+		foreign key(matricula)references funcionario(matricula));
+		
+	
+	insert into endereco(cep,rua,bairro,cidade,uf)VALUES
+	('23085-610','Rua Padre Pauwels','Campo Grande','Rio de Janeiro','RJ'),
+	('26551-090','Travessa Elpidio','Cruzeiro do Sul','Mesquita','RJ');
+
+	insert into cliente(cpf,nome,telefone,cep,numerocasa,complemento)VALUES
+	('123456789-10','Jariele','(21)99886-4587','23085-610',100,'apartamento 10');
+	
+	insert into funcionario(nome,telefone,cpf,qualificacao,experiencia,cep,numerocasa,complemento)VALUES
+	('João da Silva','(21)99869-4581','123456789-10','lanterneiro',
+	'3 anos', '26551-090',100,'casa');
+	
+	
+	
+		
+		
 	
 	
 
